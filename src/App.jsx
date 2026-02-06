76c222a70ce02000b0938e90858ca3e0f2bafe82
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-02-06 16:42:45 +0100

    add form create employee skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import { Route, Routes } from 'react-router';
 import './App.css'
 import { useState } from 'react';
-// import Modal from "react-hrnet-modal";
-// import "react-hrnet-modal/style.css";
+import EmployeeList from '../src/pages/EmployeeList'
+import CreateEmployee from '../src/pages/CreateEmployee'
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleClose() {
-    setIsOpen(false)
-  }
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<EmployeeList employees={employees} />}></Route>
-        <Route path='/create' element={<CreateEmployee onAddEmployee={addEmployee} />}></Route>
+        <Route path='/' element={<CreateEmployee />}></Route>
+        <Route path='/create' element={<EmployeeList />}></Route>
       </Routes>
-      
-      {/* <h1>HRNet</h1> */}
-      {/* <button onClick={() => setIsOpen(true)}>Ouvrir la modale</button> */}
-
-      {/* <Modal isOpen={isOpen} onClose={handleClose}>
-        Employee Created!
-      </Modal> */}
     </>
   )
 }
---
+++ src/pages/CreateEmployee.jsx
@@ -0,0 +1,134 @@
+import Modal from "react-hrnet-modal";
+import "react-hrnet-modal/style.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function CreateEmployee() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function handleClose() {
+      setIsOpen(false)
+    }
+
+    return (
+        <>
+            <h1>HRnet</h1>
+
+            <div className="create-employee-container">
+                <Link to="/create">View Current Employees</Link>
+
+                <h2>Create Employee</h2>
+
+                <form action="#" id="create-employee">
+                    <label htmlFor="first-name">First Name</label>
+                    <input type="text" id="first-name" />
+
+                    <label htmlFor="last-name">Last Name</label>
+                    <input type="text" id="last-name" />
+
+                    <label htmlFor="date-of-birth">Date of Birth</label>
+                    <input id="date-of-birth" type="date" />
+
+                    <label htmlFor="start-date">Start Date</label>
+                    <input id="start-date" type="date" />
+
+                    <fieldset className="address">
+                        <legend>Address</legend>
+
+                        <label htmlFor="street">Street</label>
+                        <input id="street" type="text" />
+
+                        <label htmlFor="city">City</label>
+                        <input id="city" type="text" />
+
+                        <label htmlFor="state">State</label>
+                        <select name="state" id="state" >
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AS">American Samoa</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FM">Federated States Of Micronesia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="GU">Guam</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MH">Marshall Islands</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="MP">Northern Mariana Islands</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PW">Palau</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="PR">Puerto Rico</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VI">Virgin Islands</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+
+                        <label htmlFor="zip-code">Zip Code</label>
+                        <input id="zip-code" type="number" />
+                    </fieldset>
+
+                    <label htmlFor="department">Department</label>
+                    <select name="department" id="department">
+                        <option>Sales</option>
+                        <option>Marketing</option>
+                        <option>Engineering</option>
+                        <option>Human Resources</option>
+                        <option>Legal</option>
+                    </select>
+      
+                </form>
+            </div>
+            
+
+
+            <button onClick={() => setIsOpen(true)}>Save</button>
+
+            <Modal isOpen={isOpen} onClose={handleClose}>
+                Employee Created!
+            </Modal>
+        </>
+    )
+}
+
+export default CreateEmployee;
