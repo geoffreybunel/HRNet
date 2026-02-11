6e07e2582ccee2003fa9df0cbe12542a025af096
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-02-11 12:59:29 +0100

    added employees list table

---
+++ src/components/EmployeeTable.jsx
@@ -0,0 +1,160 @@
+import { useReactTable, getCoreRowModel, getSortedRowModel, getFilteredRowModel, getPaginationRowModel, flexRender } from "@tanstack/react-table";
+import { useMemo, useState } from "react";
+import { format } from "date-fns";
+
+function EmployeeTable({ data }) {
+    const [sorting, setSorting] = useState([]);
+    const [globalFilter, setGlobalFilter] = useState("");
+
+    const columns = useMemo(
+        () => [
+            { accessorKey: "firstName", header: "First Name" },
+            { accessorKey: "lastName", header: "Last Name" },
+            { 
+                accessorKey: "startDate", 
+                header: "Start Date" ,
+                cell: ({ getValue }) => {
+                    const value = getValue();
+                    return value ? format(new Date(value), "MM/dd/yyyy") : "";
+                }
+            },
+            { accessorKey: "department", header: "Department" },
+            { 
+                accessorKey: "dateOfBirth", 
+                header: "Date of Birth",
+                cell: ({ getValue }) => {
+                    const value = getValue();
+                    return value ? format(new Date(value), "MM/dd/yyyy") : "";
+                }
+            },
+            { accessorKey: "street", header: "Street" },
+            { accessorKey: "city", header: "City" },
+            { accessorKey: "state", header: "State" },
+            { accessorKey: "zipCode", header: "Zip Code" },
+        ],
+        []
+    );
+
+    const table = useReactTable({
+        data,
+        columns,
+        state: {
+          sorting,
+          globalFilter,
+        },
+        onSortingChange: setSorting,
+        onGlobalFilterChange: setGlobalFilter,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+    });
+    
+    return (
+        <div className="employee-table_wrapper">
+            {/* Top controls */}
+            <div className="table-controls">
+                <div>
+                    Show{" "}
+                    <select
+                        value={table.getState().pagination.pageSize}
+                        onChange={(e) => table.setPageSize(Number(e.target.value))}
+                        id="entries"
+                    >
+                        {[5, 10, 25, 50].map((size) => (
+                            <option key={size} value={size}>
+                                {size}
+                            </option>
+                        ))}
+                    </select>{" "}
+                    entries
+                </div>
+
+                <div>
+                    Search:{" "}
+                    <input
+                        value={globalFilter ?? ""}
+                        onChange={(e) => setGlobalFilter(e.target.value)}
+                        id="searchBar"
+                    />
+                </div>
+            </div>
+
+            {/* Table */}
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th
+                                    key={header.id}
+                                    onClick={header.column.getToggleSortingHandler()}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext()
+                                    )}
+                                    {{
+                                        asc: " 🔼",
+                                        desc: " 🔽",
+                                    }[header.column.getIsSorted()] ?? ""}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+
+                <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id}>
+                                    {flexRender(
+                                        cell.column.columnDef.cell ??
+                                        cell.column.columnDef.accessorKey,
+                                        cell.getContext()
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div className="table-lower-controls">
+                {/* Entry counter */}
+                <div>
+                    Showing{" "}
+                    {table.getState().pagination.pageIndex *
+                    table.getState().pagination.pageSize +
+                    1}{" "}
+                    to{" "}
+                    {Math.min(
+                    (table.getState().pagination.pageIndex + 1) *
+                        table.getState().pagination.pageSize,
+                    data.length
+                    )}{" "}
+                    of {data.length} entries
+                </div>
+
+                {/* Pagination */}
+                <div className="pagination">
+                    <button onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                        Previous
+                    </button>
+
+                    <span>
+                        Page {table.getState().pagination.pageIndex + 1} of{" "}
+                        {table.getPageCount()}
+                    </span>
+
+                    <button onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                        Next
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default EmployeeTable
--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import EmployeeTable from "../components/EmployeeTable";
 
-function EmployeeList() {
+function EmployeeList({ employees }) {
     return (
         <>
         <h1>Current Employees</h1>
 
         <div className="main-container">
-
+            <EmployeeTable data={employees} />
 
             <Link to="/">Home</Link>
         </div>
