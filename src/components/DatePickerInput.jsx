f7f3ff53bbfd8cc4ee2153f356b0168e3b40f8c0
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2026-02-12 22:05:27 +0100

    changed icons for better performance

--- src/components/DatePickerInput.jsx
+++ src/components/DatePickerInput.jsx
@@ -30,14 +30,7 @@ export default function DatePickerInput({ label, selectedDate, onChange, id }) {
           prevMonthButtonDisabled,
           nextMonthButtonDisabled
         }) => (
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "6px",
-              justifyContent: "center"
-            }}
-          >
+          <div className="dp-header">
             {/* < */}
             <button
               type="button"
@@ -45,7 +38,9 @@ export default function DatePickerInput({ label, selectedDate, onChange, id }) {
               onClick={decreaseMonth}
               disabled={prevMonthButtonDisabled}
             >
-              <i className="fa-solid fa-caret-left"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="dp-icon">
+                <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+              </svg>
             </button>
 
             {/* 🏠 Home */}
@@ -55,7 +50,11 @@ export default function DatePickerInput({ label, selectedDate, onChange, id }) {
               onClick={() => onChange(new Date())}
               title="Go to today"
             >
-              <i className="fa-solid fa-house"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="dp-icon">
+                <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+              </svg>
+
             </button>
 
             {/* Mois */}
@@ -93,7 +92,10 @@ export default function DatePickerInput({ label, selectedDate, onChange, id }) {
               onClick={increaseMonth}
               disabled={nextMonthButtonDisabled}
             >
-              <i className="fa-solid fa-caret-right"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="dp-icon">
+                <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+              </svg>
+
             </button>
           </div>
         )}
--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -110,8 +110,16 @@ function EmployeeTable({ data }) {
                                         header.getContext()
                                     )}
                                     {{
-                                        asc: <i class="fa-solid fa-caret-up ml-5"></i>,
-                                        desc: <i class="fa-solid fa-caret-down ml-5"></i>,
+                                        asc: 
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="ml-5 table-icon">
+                                                <path fillRule="evenodd" d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z" clipRule="evenodd" />
+                                            </svg>
+                                      ,
+                                        desc: 
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="ml-5 table-icon">
+                                                <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
+                                            </svg>
+                                      ,
                                     }[header.column.getIsSorted()] ?? ""}
                                 </th>
                             ))}
